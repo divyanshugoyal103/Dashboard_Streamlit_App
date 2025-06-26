d99aec8a7068899cb956357e7306eba511c3ed49
divyanshugoyal103 <67494831+divyanshugoyal103@users.noreply.github.com>
2025-06-25 22:23:20 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,10 +2,12 @@ import pandas as pd
 
 def load_csv(uploaded_file):
     try:
-        df = pd.read_csv(uploaded_file)
+        df = pd.read_csv(uploaded_file, parse_dates=True, infer_datetime_format=True)
         return df, None
     except Exception as e:
         return None, str(e)
 
 def get_column_types(df):
-    return df.select_dtypes(include='number').columns.tolist(), df.select_dtypes(include='object').columns.tolist()
+    num_cols = df.select_dtypes(include='number').columns.tolist()
+    cat_cols = df.select_dtypes(include='object').columns.tolist()
+    return num_cols, cat_cols
