65ef46d70a20b287bf5f1f0da182366f58f46fe6
divyanshugoyal103 <67494831+divyanshugoyal103@users.noreply.github.com>
2025-08-07 23:08:11 -0400

    Delete resume optimizer directory

--- resume optimizer/utilis/keyword_matcher.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def extract_keywords(text):
-    words = re.findall(r'\b\w+\b', text.lower())
-    return set(words)
-
-def find_missing_keywords(resume_keywords, jd_keywords):
-    return list(jd_keywords - resume_keywords)
